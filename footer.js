37be841f065fca8bbc981e51bb4baf425fab845a
mohammed qasim <87497268+MohammedTsmu@users.noreply.github.com>
2023-04-09 18:43:29 +0300

    version updated in footer

--- footer.js
+++ footer.js
@@ -1,3 +1,3 @@
 const year = new Date().getFullYear();
 const footerText = document.getElementById("footerText");
-footerText.innerHTML = `<span class="orange">Mohammed-qasim</span> &copy; ${year} all rights reserved`;
+footerText.innerHTML = `<span class="orange">Mohammed-qasim</span> &copy; ${year} all rights reserved v1.0.1`;
